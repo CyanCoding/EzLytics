f4845f9c1362b97f33a82910832d6f3b4293c594
Camden <CyanCoding@users.noreply.github.com>
2020-10-18 16:19:03 -0400

    Add ProgramName Variable
    Added an optional value for a human-friendly name for the program.

--- EzLytics NuGet/EzLytics NuGet/Tracking.cs
+++ EzLytics NuGet/EzLytics NuGet/Tracking.cs
@@ -7,34 +7,45 @@ using System.Threading;
 
 namespace EzLyticsSDK {
     class Tracking {
+        readonly string TEMP_FOLDER = Path.Combine(Path.GetTempPath() + "EzLytics\\");
+
         /// <summary>
         /// Creates a new EzLytics file.
         /// </summary>
         /// <returns>File path of the new EzLytics file.</returns>
-        public string Start() {
-            string randomFile = string.Format(@"{0}.ezl", Guid.NewGuid());
-            string filePath = "";
-
+        public string GenerateRandomFile() {
             try {
-                filePath = Path.Combine(Path.GetTempPath(), randomFile);
+                if (!Directory.Exists(TEMP_FOLDER)) {
+                    Directory.CreateDirectory(TEMP_FOLDER);
+                }
             }
-            catch (SecurityException) {
-                throw new Exception("Could not access the EzLytics temp folder.");
+            catch (Exception) {
+                // Could not write to that folder
+                throw new Exception("Failed to access the EzLytics temp folder. (Error 200)");
             }
+
+            string randomFile = string.Format(@"{0}.ezl", Guid.NewGuid());
+            string filePath = Path.Combine(TEMP_FOLDER, randomFile);
 
             return filePath;
         }
+
+        // TODO: We need to create a folder for all of the EzLytics files because how can we find it again if the program crashes? Also delete them when we send data?
 
         /// <summary>
         /// Starts the program analysis.
         /// </summary>
         /// <param name="path">The path to the EzLytics file.</param>
-        public void StartRecording(string path) {
+        /// <param name="programName">A human friendly program name. (optional)</param>
+        public void StartRecording(string path, string programName) {
             string recordType = "AUTO";
             string flag = "program_start";
             string message = "The program has started.";
             string date = DateTime.Now.ToString();
-            string programName = System.AppDomain.CurrentDomain.FriendlyName;
+
+            if (programName == null || programName == "") {
+                programName = AppDomain.CurrentDomain.FriendlyName;
+            }
 
             // Example output:
             // ["AUTO", "program_start", "The program has started.", "10/18/2020 00:00:00", "Goal Getter"]
@@ -52,7 +63,7 @@ namespace EzLyticsSDK {
                 streamWriter.Close();
             }
             catch (IOException) {
-                throw new Exception("Unable to access" + path + " EzLytics file.");
+                throw new Exception("Unable to access" + path + " EzLytics file. (Error 201)");
             }
         }
     }
